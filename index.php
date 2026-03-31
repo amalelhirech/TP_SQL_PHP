4e4a43f8ca914701ffcf32f6313a2c54d07b0a47
EL HIRECH Amal <elhirechamal@icloud.com>
2026-03-31 17:43:41 +0200

    Partie 2 - 4 bonus : affichage des prof via jointures

--- index.php
+++ index.php
@@ -44,6 +44,34 @@ foreach($profs as $prof) {
 
 echo "</ul>";
 
+
+$resultat = $dbPDO->prepare("
+    SELECT 
+        p.nom AS nom_prof,
+        p.prenom AS prenom_prof,
+        m.libelle AS nom_matiere,
+        c.nom_classe AS nom_classe
+    FROM prof p
+    INNER JOIN matiere m ON p.id_matiere = m.id
+    INNER JOIN classes c ON p.id_classe = c.id
+");
+$resultat->execute();
+
+$profs_infos = $resultat->fetchAll(PDO::FETCH_OBJ);
+
+echo "<br>Liste des professeurs avec leur matière et leur classe :<ul>";
+
+foreach($profs_infos as $prof) {
+    echo "<li>" 
+        . $prof->nom_prof . " " . $prof->prenom_prof
+        . " - Matière : " . $prof->nom_matiere
+        . " - Classe : " . $prof->nom_classe
+        . "</li>";
+}
+
+echo "</ul>";
+
+
 ?>
 
 
